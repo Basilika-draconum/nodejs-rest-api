cf8ea274d81533095301372fcce203c09bbb9459
Lika <14dubinenkoanzhelika@gmail.com>
2023-04-18 16:07:36 +0300

    add verification stage

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -9,28 +9,30 @@ const {
   logoutUser,
   updateSubscriptionUser,
   updateAvatarUser,
+  verifyEmail,
+  resendEmail,
 } = require("../../controllers/auth");
 const { authenticate, upload } = require("../../middlewares");
 
+// sign up
 router.post(
   "/users/register",
   validateBody(schemas.registerSchema),
   registerUser
 );
+router.get("/users/verify/:verificationToken", verifyEmail);
+router.post("/users/verify", validateBody(schemas.verifySchema), resendEmail);
 
+// sign in
 router.post("/users/login", validateBody(schemas.loginSchema), loginUser);
-
 router.get("/users/current", authenticate, getCurrentUser);
-
 router.post("/users/logout", authenticate, logoutUser);
-
 router.patch(
   "/users",
   authenticate,
   validateBody(schemas.updateSubscriptionSchema),
   updateSubscriptionUser
 );
-
 router.patch(
   "/users/avatars",
   authenticate,
